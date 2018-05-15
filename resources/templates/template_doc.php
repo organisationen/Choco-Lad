a22f8d56dfc6ba169f03ea0a4aea240466f2861f
DrivableJonatan <38322093+DrivableJonatan@users.noreply.github.com>
2018-05-15 21:35:37 +0200

    Initial commit

---
+++ resources/templates/template_doc.php
@@ -0,0 +1,12 @@
+<?php
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Cache-Control: no-cache");
+header("Pragma: no-cache");
+
+$title = 'okonfigurerad titel';
+
+require($_SERVER['DOCUMENT_ROOT'] . '/resources/templates/start.php);
+require($_SERVER['DOCUMENT_ROOT'] . '/resources/templates/head.php);
+require($_SERVER['DOCUMENT_ROOT'] . '/resources/templates/end.php);
+//made by Jonatan Nogrell, I guess;
+?>
