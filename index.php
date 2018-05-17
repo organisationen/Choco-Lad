d231b8471bdcb5176647e4b846a9edbb14b2c1a4
DrivableJonatan <38322093+DrivableJonatan@users.noreply.github.com>
2018-05-17 17:47:21 +0200

    lite mer grejer

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-header('http://www.rl.se');
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Cache-Control: no-cache");
+header("Pragma: no-cache");
+
+session_start();
+
+$title = 'Välkommen till Choco Lad!';
+
+require($_SERVER['DOCUMENT_ROOT'] . '/resources/templates/start.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/resources/templates/head.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/resources/templates/header.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/resources/templates/main_start.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/resources/templates/end.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/resources/templates/footer.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/resources/templates/end.php');
+//made by Jonatan Nogrell, I guess;
 ?>
--- resources/templates/template_doc.php
+++ resources/templates/template_doc.php
@@ -3,10 +3,16 @@ header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header("Cache-Control: no-cache");
 header("Pragma: no-cache");
 
+session_start();
+
 $title = 'okonfigurerad titel';
 
 require($_SERVER['DOCUMENT_ROOT'] . '/resources/templates/start.php');
 require($_SERVER['DOCUMENT_ROOT'] . '/resources/templates/head.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/resources/templates/header.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/resources/templates/main_start.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/resources/templates/end.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/resources/templates/footer.php');
 require($_SERVER['DOCUMENT_ROOT'] . '/resources/templates/end.php');
 //made by Jonatan Nogrell, I guess;
 ?>
