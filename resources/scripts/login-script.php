851c77083749f3ac1185464c339075a71ca006d6
DrivableJonatan <38322093+DrivableJonatan@users.noreply.github.com>
2018-05-25 19:51:51 +0200

    Revert "Update login-script.php"

--- resources/scripts/login-script.php
+++ resources/scripts/login-script.php
@@ -4,23 +4,28 @@
     
         if (isset($_POST['submit'])) {
             
-            if (isset($_POST['username']) && isset($_POST['password'])) {
+            if (isset($_POST['username']) || isset($_POST['password'])) {
                 
                 $username = $_POST['username'];
                 $hashed_password = mysqli_real_escape_string($connection, hash('sha512', $_POST['password']));
+                
+               
+                
+                if($username && $hashed_password) {
+                    $query = "SELECT * FROM users WHERE Username = '$username' AND Password = '$hashed_password'";
+                    $result = mysqli_query($connection, $query);
                     
-                $query = "SELECT * FROM users WHERE Username = '$username' AND Password = '$hashed_password'";
-                $result = mysqli_query($connection, $query);
-                    
-                if(mysqli_num_fields($connection, $result) > 0) {
+                    if (mysqli_fetch_array($result)){
                         $_SESSION['loggedIn'] = true;
                         $_SESSION['username'] = $username;
                         echo $username;
-                } else {
-                        //Kunde inte hitta en användare i databasen med den kombinationen av användarnamn och lösenord        
-                }                
-            } else {
-                $error = '<p class="bg-danger text-danger">Var god fyll i båda fälten</p>';
+                    } else if (mysqli_num_fields($result)){
+                        $error = '<p class="bg-danger text-danger">Var god fyll i båda fälten</p>';
+                    } else {
+                        
+                    }
+                } 
+                
             }
         }
 ?>
